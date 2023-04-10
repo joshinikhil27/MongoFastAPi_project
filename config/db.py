64fe8ee230f785fe57fa3818ab0c5665fb68f0ff
joshinikhil27 <joshi.nikhil.27@gmail.com>
2023-04-10 19:18:57 +0530

    Update db.py

--- config/db.py
+++ config/db.py
@@ -1,2 +1,2 @@
 from pymongo import MongoClient
-connection = MongoClient('mongodb://root:mongodbpassword@cluster0.nuuitgb.mongodb.net')
+connection = MongoClient('mongodb://rootuser:password123@documentdbcluster.cluster-c4z3dvcijoaa.us-east-1.docdb.amazonaws.com:27017/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false')
